51babd389cc1be2b4dae735e531fc1dbab88b8e4
eexsty <nekkan@protonmail.ch>
2022-02-21 15:39:20 -0300

    Add an initial class to the unfinished configuration system

--- kraken/purpur/src/main/kotlin/org/hexalite/network/kraken/configuration/KrakenConfig.kt
+++ kraken/purpur/src/main/kotlin/org/hexalite/network/kraken/configuration/KrakenConfig.kt
@@ -1,4 +1,10 @@
 package org.hexalite.network.kraken.configuration
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 @Serializable
-data class KrakenConfig
+data class KrakenConfig(
+    @SerialName("enable_debug_log_level")
+    val enableDebugLogLevel: Boolean = false,
+)
