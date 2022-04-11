a32e8f56565393b9d53c01600e8fbd32c480ece7
eexsty <hello@exst.fun>
2022-04-11 17:54:37 -0300

    Add CORS support Co-authored-by: Gabriel <SrGaabriel@users.noreply.github.com>

--- rest-webserver/src/bootstrap/server.rs
+++ rest-webserver/src/bootstrap/server.rs
@@ -2,11 +2,24 @@ use std::{io::Error, net::SocketAddr, sync::Arc};
 
 use crate::api;
 use crate::app::WebserverStateData;
-use actix_web::{middleware::Logger, web::Data as AppData, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{http::header, middleware::Logger, web::Data as AppData, App, HttpServer};
 
 pub async fn build(state: WebserverStateData, ip: SocketAddr) -> Result<(), Error> {
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default()
+                    .allowed_origin(format!("http://localhost:{}", ip.port()).as_str())
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![
+                        header::AUTHORIZATION,
+                        header::ACCEPT,
+                        header::CONTENT_TYPE,
+                    ])
+                    .supports_credentials()
+                    .max_age(3600),
+            )
             .wrap(Logger::default())
             .app_data(AppData::new(Arc::new(state.to_owned())))
             .service(api::v1())
