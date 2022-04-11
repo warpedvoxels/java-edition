3acb6f5d13ba2424c901bf73925c262f8f10dfad
eexsty <hello@exst.fun>
2022-04-11 18:17:29 -0300

    Add initial code for authentication
    Co-authored-by: Gabriel <SrGaabriel@users.noreply.github.com>

---
+++ rest-webserver/src/middleware/authentication.rs
@@ -0,0 +1,58 @@
+use std::future::{ready, Ready};
+
+use actix_web::{
+    body::EitherBody,
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+    Error, HttpResponse,
+};
+use futures_util::future::LocalBoxFuture;
+
+pub struct Authentication;
+
+impl<S, B> Transform<S, ServiceRequest> for Authentication
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<EitherBody<B>>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = AuthenticationMiddleware<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(AuthenticationMiddleware { service }))
+    }
+}
+
+pub struct AuthenticationMiddleware<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for AuthenticationMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<EitherBody<B>>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, request: ServiceRequest) -> Self::Future {
+        let authenticated = true;
+        if !authenticated {
+            let (request, _) = request.into_parts();
+            let response = HttpResponse::Unauthorized().finish().map_into_right_body();
+
+            return Box::pin(async { Ok(ServiceResponse::new(request, response)) });
+        }
+        let response = self.service.call(request);
+        Box::pin(async move {
+            response.await.map(ServiceResponse::map_into_left_body)
+        })
+    }
+}
--- rest-webserver/src/middleware/mod.rs
+++ rest-webserver/src/middleware/mod.rs
@@ -1,1 +1,2 @@
-pub mod authorization;
+mod authentication;
+pub use authentication::*;
