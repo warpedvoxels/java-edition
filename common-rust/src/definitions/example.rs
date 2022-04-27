eb5ad8b82397e31cb68609811294aa4fcf9d76d8
eexsty <hello@exst.fun>
2022-04-27 13:07:46 -0300

    docs: add an optional field to protobuf example [skip ci]

--- common-rust/src/definitions/example.rs
+++ common-rust/src/definitions/example.rs
@@ -2,10 +2,10 @@
 #[serde(rename_all = "snake_case")]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Example {
-    #[prost(string, required, tag="1")]
-    pub name: ::prost::alloc::string::String,
-    #[prost(int32, required, tag="2")]
+    #[prost(string, optional, tag="1")]
+    pub name: ::core::option::Option<::prost::alloc::string::String>,
+    #[prost(int32, tag="2")]
     pub id: i32,
-    #[prost(string, required, tag="3")]
+    #[prost(string, tag="3")]
     pub email: ::prost::alloc::string::String,
 }
