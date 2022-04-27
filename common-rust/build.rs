ec7047b2fa6e32863c6374d0975fa4d6d8d33d5c
eexsty <hello@exst.fun>
2022-04-27 11:51:07 -0300

    feat(common): include all possible definitions

--- common-rust/build.rs
+++ common-rust/build.rs
@@ -10,12 +10,9 @@ fn main() {
         .unwrap();
 
     let mut files: Vec<PathBuf> = Vec::new();
-    for receiver in ["", "entity/", "protocol/", "rest/"] {
-        let path = format!("../definitions/{}*.proto", receiver);
-        if let Ok(input) = glob::glob(&path) {
-            let input = input.filter_map(|value| value.unwrap().canonicalize().ok());
-            files.extend(input);
-        }
+    if let Ok(input) = glob::glob("../definitions/**/*.proto") {
+        let input = input.filter_map(|value| value.unwrap().canonicalize().ok());
+        files.extend(input);
     }
     println!("Files: {}", files.len());
 
@@ -23,7 +20,6 @@ fn main() {
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
         .type_attribute(".", "#[serde(rename_all = \"snake_case\")]")
         .out_dir("src/definitions")
-
         .compile_protos(&files, &[working_directory])
         .unwrap();
 }
