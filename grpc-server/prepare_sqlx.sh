83a2b608a038c724adf1fcb5c8bc9f28cb58ddf1
eexsty <hello@exst.fun>
2022-05-10 14:15:19 -0300

    feat(grpc): add support for offline sqlx

---
+++ grpc-server/prepare_sqlx.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Database needs to be running before running this script. You can shut down
+# the database after running this.
+
+SETTINGS="$(cat ~/.hexalite/settings.toml)"
+SETTINGS="${SETTINGS#*\[grpc.services.postgres\]}" 
+SETTINGS=$(echo "$SETTINGS" | tail -n +2 | sed -e '/^$/,$d')
+
+get_field() {
+    echo "$SETTINGS" | awk -F ' = ' "/$1/ {print \$2}" | tr -d "'"
+}
+
+# Fields: host, port, user, password, database
+HOST=`get_field host`
+PORT=`get_field port`
+USER=`get_field user`
+PASSWORD=`get_field password`
+DATABASE=`get_field database`
+export DATABASE_URL="postgres://$USER:$PASSWORD@$HOST:$PORT/$DATABASE"
+
+if [ "$1" != "-s" ]; then
+    cargo install sqlx-cli
+fi
+
+cargo sqlx prepare -- --lib
