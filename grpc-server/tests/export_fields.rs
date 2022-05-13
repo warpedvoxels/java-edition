afbcff846fb3ca5bce687b051da93449bf94efae
eexsty <hello@exst.fun>
2022-05-13 16:20:48 -0300

    tests(grpc): add a test for testing exported fields output

---
+++ grpc-server/tests/export_fields.rs
@@ -0,0 +1,17 @@
+#![feature(const_type_name)]
+
+#[allow(dead_code)]
+#[derive(hexalite_common::ExportFields)]
+pub struct Hello {
+    hello: Option<String>,
+    world: Result<(), Option<Vec<String>>>,
+}
+
+#[test]
+pub fn test_field_exporting_output() {
+    let fields = Hello::fields();
+    println!("{:?}", fields);
+
+    assert_eq!(fields["hello"], "core::option::Option<alloc::string::String>");
+    assert_eq!(fields["world"], "core::result::Result<(), core::option::Option<alloc::vec::Vec<alloc::string::String>>>");
+}
