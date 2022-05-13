a3c4ac4724d807edbf0a47c9757823d896115289
eexsty <hello@exst.fun>
2022-05-13 16:07:43 -0300

    tests(grpc): move username tests to `tests/`

--- grpc-server/src/datatype/username.rs
+++ grpc-server/src/datatype/username.rs
@@ -39,12 +39,3 @@ impl AsRef<String> for Username {
     }
 }
 
-#[test]
-pub fn test_username() {
-    let username = Username::from("my_username");
-    assert_eq!(username.value().unwrap(), "my_username");
-    assert!(Username::from("my_username_is_too_long_aaaa").value().is_err());
-
-    let serialized = serde_json::to_string(&username).unwrap(); 
-    assert_eq!(serialized, r#""my_username""#);
-}
